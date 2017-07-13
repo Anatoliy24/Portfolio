a13c25fd1fd60c1e10379c7c0e8fdc2cb3cd45d9
Anatoliy Kapp <Anatoliy_kapp@mail.ru>
2017-07-14 01:43:03 +0300

    13.07.17_1

--- public/js/about/about.js
+++ public/js/about/about.js
@@ -4,7 +4,38 @@ jQuery(document).ready(function ($) {
     let fixed_offset = 0;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
     e.preventDefault();
-
-
   });
+
+  //Fullscreen
+  $(".header__hamburger").on('click', function(e){
+    e.preventDefault();
+    $(".fullscreen").css("display", "block");
+    $(".header__hamburger").css("display", "none")
+  });
+
+  $(".fullscreen-cross").on('click', function(e){
+    e.preventDefault();
+    $(".fullscreen").css("display", "none");
+    $(".header__hamburger").css("display", "block")
+  });
+
+  $(".welcome__button_authorization").on('click', function(e){
+    e.preventDefault();
+    $(".welcome__form").css("transform", "rotateY(180deg)");
+    $(".login__form").css("transform", "rotateY(0deg)")
+  });
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
